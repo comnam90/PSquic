cba6964acb204e9049bed9046277420abc701e53
Ben Thomas <2719284+comnam90@users.noreply.github.com>
2025-08-09 16:46:56 +1200

    refactor: Remove unused script imports from run-tests.ps1

--- run-tests.ps1
+++ run-tests.ps1
@@ -1,7 +1,2 @@
-. "/Users/benthomas/git/github/comnam90/PSquic/Source/Public/Connect-PSquic.ps1"
-. "/Users/benthomas/git/github/comnam90/PSquic/Source/Private/Invoke-PSquicRestMethod.ps1"
-. "./Source/Public/Connect-PSquic.ps1"
-. "./Source/Private/Invoke-PSquicRestMethod.ps1"
-. "./Source/Private/Test-Func.ps1"
-
+# Run Pester tests
 Invoke-Pester -Path ./Tests -Output Detailed
