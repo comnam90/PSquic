fead390adea40e31031d572616a71c2d25528073
Ben Thomas <2719284+comnam90@users.noreply.github.com>
2025-08-09 16:40:26 +1200

    feat: Update documentation for Get-PSquicServices with additional examples and clarifications

--- Source/Public/Get-PSquicServices.ps1
+++ Source/Public/Get-PSquicServices.ps1
@@ -5,17 +5,47 @@ function Get-PSquicServices {
     
     .DESCRIPTION
         This function queries the Quic API to retrieve the list of service identifiers 
-        that the authenticated user is authorized to access. Requires a valid API key 
-        to be set using Connect-PSquic.
+        that the authenticated user is authorized to access. The service identifiers can
+        then be used with Get-PSquicSession to retrieve detailed session information.
+        Requires a valid API key to be set using Connect-PSquic.
     
     .EXAMPLE
         PS> Get-PSquicServices
         
         Returns an array of service identifiers that the user can access.
     
+    .EXAMPLE
+        PS> $services = Get-PSquicServices
+        PS> Write-Host "You have access to $($services.Count) services: $($services -join ', ')"
+        
+        Retrieves services and displays count and list.
+    
+    .EXAMPLE
+        PS> Get-PSquicServices | Get-PSquicSession
+        
+        Uses pipeline to get session information for all authorized services.
+    
+    .INPUTS
+        None
+        You cannot pipe objects to this function.
+    
     .OUTPUTS
         System.String[]
-        An array of service identifiers.
+        An array of service identifiers that can be used with other PSquic functions.
+    
+    .NOTES
+        - Requires an active connection established with Connect-PSquic
+        - The returned service IDs can be used with Get-PSquicSession
+        - This function supports pipeline output to other PSquic functions
+    
+    .LINK
+        Connect-PSquic
+    
+    .LINK
+        Get-PSquicSession
+    
+    .LINK
+        Disconnect-PSquic
     #>
     [CmdletBinding()]
     param()
